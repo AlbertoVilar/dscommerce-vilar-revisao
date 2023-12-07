102626c9c419058f57a22d67eafb7b0d019911af
AlbertoVilar <albertovilar1@gmail.com>
2023-12-07 16:08:03 -0300

    Second commit Payment

--- src/main/java/com/dev/dscommerce/entities/Payment.java
+++ src/main/java/com/dev/dscommerce/entities/Payment.java
@@ -8,26 +8,20 @@ import lombok.Setter;
 
 import java.time.Instant;
 @Entity
-@Table(name = "tb_orders")
+@Table(name = "tb_payments")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class Order {
+public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private OrderStatus status;
 
-    //Here we respect the model UML, the attribute is igual as descriting.
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
-
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
-
+    @OneToOne
+    @MapsId
+    private Order order;
 
 }
